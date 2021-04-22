896efc4f29bdae67b99434f6f9cd73fb599eeac9
ilyaShevchuk <58067954+ilyaShevchuk@users.noreply.github.com>
2021-04-22 11:50:00 +0300

    Lab1 (#1)
    * Commit 1
    
    * Commit 2
    
    * Commit 3
    
    * Commit 4
    
    * Commit 5
    
    * Commit 6
    
    * Commit 7
    
    * Commit 8
    
    * Commit 9
    
    * Commit 10
    
    * Commit 10
    
    * Commit after Elina's corrections
    
    * Commit after Elina's corrections
    
    * Commit after Elina's corrections
    
    * Commit 12
    
    * Commit 12
    
    * Commit 14
    
    * Commit 15
    
    * Commit 15
    
    Co-authored-by: ilyaShevchuk <ilyaShevchuk@users.noreply.github.com>

--- src/main/java/com/itmo/java/basics/logic/impl/SegmentImpl.java
+++ src/main/java/com/itmo/java/basics/logic/impl/SegmentImpl.java
@@ -1,9 +1,19 @@
 package com.itmo.java.basics.logic.impl;
 
-import com.itmo.java.basics.logic.Segment;
 import com.itmo.java.basics.exceptions.DatabaseException;
+import com.itmo.java.basics.index.impl.SegmentIndex;
+import com.itmo.java.basics.index.impl.SegmentOffsetInfoImpl;
+import com.itmo.java.basics.logic.DatabaseRecord;
+import com.itmo.java.basics.logic.Segment;
+import com.itmo.java.basics.logic.WritableDatabaseRecord;
+import com.itmo.java.basics.logic.io.DatabaseInputStream;
+import com.itmo.java.basics.logic.io.DatabaseOutputStream;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Optional;
 
@@ -15,8 +25,24 @@ import java.util.Optional;
  * - является неизменяемым после появления более нового сегмента
  */
 public class SegmentImpl implements Segment {
+    private static final int maxSize = 100000;
+    private final Path pathForWriting;
+    private final SegmentIndex indexMap;
+    private long offset;
+
+    private SegmentImpl(Path segPath) {
+        indexMap = new SegmentIndex();
+        pathForWriting = segPath;
+        offset = 0;
+    }
+
     static Segment create(String segmentName, Path tableRootPath) throws DatabaseException {
-        throw new UnsupportedOperationException(); // todo implement
+        try {
+            return new SegmentImpl(Files.createFile(Path.of(tableRootPath.toString() + File.separator
+                    + segmentName)));
+        } catch (IOException e) {
+            throw new DatabaseException("Segment create fail", e);
+        }
     }
 
     static String createSegmentName(String tableName) {
@@ -25,26 +51,61 @@ public class SegmentImpl implements Segment {
 
     @Override
     public String getName() {
-        return null;
+        return pathForWriting.getFileName().toString();
+    }
+
+    private int writeWithStream(WritableDatabaseRecord record) throws IOException {
+        try (DatabaseOutputStream stream = new DatabaseOutputStream(new FileOutputStream(pathForWriting.toString(), true))) {
+            indexMap.onIndexedEntityUpdated(new String(record.getKey()), new SegmentOffsetInfoImpl(offset));
+            return stream.write(record);
+        }
     }
 
     @Override
     public boolean write(String objectKey, byte[] objectValue) throws IOException {
-        return false;
+        WritableDatabaseRecord record;
+        if (objectValue == null) {
+            record = new RemoveDatabaseRecord(objectKey.getBytes());
+        } else {
+            record = new SetDatabaseRecord(objectKey.getBytes(), objectValue);
+        }
+        if (isReadOnly()) {
+            return false;
+        }
+        offset += writeWithStream(record);
+        return true;
     }
 
     @Override
     public Optional<byte[]> read(String objectKey) throws IOException {
-        return Optional.empty();
+        var currentOffset = indexMap.searchForKey(objectKey);
+        if (currentOffset.isEmpty()) {
+            return Optional.empty();
+        }
+        try (var fileStream = new FileInputStream(pathForWriting.toString())) {
+            long skipped = fileStream.skip(currentOffset.get().getOffset());
+            if (skipped != currentOffset.get().getOffset()) {
+                throw new IOException("File(Segment) problems: Can't skip offset bytes");
+            }
+            try (var stream = new DatabaseInputStream(fileStream)) {
+                var readRecord = stream.readDbUnit();
+                return readRecord.map(DatabaseRecord::getValue);
+            }
+        }
     }
 
     @Override
     public boolean isReadOnly() {
-        return false;
+        return pathForWriting.toFile().length() >= maxSize;
     }
 
     @Override
     public boolean delete(String objectKey) throws IOException {
-        return false;
+        if (isReadOnly()) {
+            return false;
+        }
+        writeWithStream(new RemoveDatabaseRecord(objectKey.getBytes()));
+        return true;
     }
+
 }
