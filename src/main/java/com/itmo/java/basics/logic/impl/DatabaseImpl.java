ec34f887861137887c5276248b55e18d7ea6fcd3
ilyaShevchuk <58067954+ilyaShevchuk@users.noreply.github.com>
2021-04-22 11:50:00 +0300

    Lab1 (#1)
    * Commit 1
    
    * Commit 2
    
    * Commit 3
    
    * Commit 4
    
    * Commit 5
    
    * Commit 6
    
    * Commit 7
    
    * Commit 8
    
    * Commit 9
    
    * Commit 10
    
    * Commit 10
    
    * Commit after Elina's corrections
    
    * Commit after Elina's corrections
    
    * Commit after Elina's corrections
    
    * Commit 12
    
    * Commit 12
    
    * Commit 14
    
    * Commit 15
    
    * Commit 15
    
    Co-authored-by: ilyaShevchuk <ilyaShevchuk@users.noreply.github.com>

--- src/main/java/com/itmo/java/basics/logic/impl/DatabaseImpl.java
+++ src/main/java/com/itmo/java/basics/logic/impl/DatabaseImpl.java
@@ -1,38 +1,83 @@
 package com.itmo.java.basics.logic.impl;
 
 import com.itmo.java.basics.exceptions.DatabaseException;
+import com.itmo.java.basics.index.impl.TableIndex;
 import com.itmo.java.basics.logic.Database;
+import com.itmo.java.basics.logic.Table;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
 public class DatabaseImpl implements Database {
+    private static final Map<String, Table> tablesMap = new HashMap<>();
+    private final String name;
+    private final Path baseDir;
+
+    private DatabaseImpl(String dbName, Path databaseRoot) throws IOException {
+        name = dbName;
+        baseDir = Paths.get(databaseRoot.toString() + File.separator + dbName);
+        Files.createDirectory(baseDir);
+    }
+
     public static Database create(String dbName, Path databaseRoot) throws DatabaseException {
-        return null;
+        if (dbName == null) {
+            throw new DatabaseException("DbName is null");
+        }
+        try {
+            return new DatabaseImpl(dbName, databaseRoot);
+        } catch (IOException e) {
+            throw new DatabaseException("Failed to create database ", e);
+        }
     }
 
     @Override
     public String getName() {
-        return null;
+        return name;
     }
 
     @Override
     public void createTableIfNotExists(String tableName) throws DatabaseException {
+        Table table;
+        try {
+            if (tableName == null) {
+                throw new DatabaseException("Table name is null");
+            }
+            table = TableImpl.create(tableName, baseDir, new TableIndex());
 
+        } catch (DatabaseException e) {
+            throw new DatabaseException("Can not create table with name " + tableName, e);
+        }
+        tablesMap.put(tableName, table);
     }
 
     @Override
     public void write(String tableName, String objectKey, byte[] objectValue) throws DatabaseException {
-
+        if (!tablesMap.containsKey(tableName)) {
+            throw new DatabaseException("There are not table with name" + tableName);
+        }
+        tablesMap.get(tableName).write(objectKey, objectValue);
     }
 
     @Override
     public Optional<byte[]> read(String tableName, String objectKey) throws DatabaseException {
-        return Optional.empty();
+        if (!tablesMap.containsKey(tableName)) {
+            throw new DatabaseException("There are not table with name" + tableName);
+        }
+        return tablesMap.get(tableName).read(objectKey);
     }
 
     @Override
     public void delete(String tableName, String objectKey) throws DatabaseException {
+        if (!tablesMap.containsKey(tableName)) {
+            throw new DatabaseException("There are not table with name" + tableName);
+        }
+        tablesMap.get(tableName).delete(objectKey);
 
     }
 }
--- src/main/java/com/itmo/java/basics/logic/impl/RemoveDatabaseRecord.java
+++ src/main/java/com/itmo/java/basics/logic/impl/RemoveDatabaseRecord.java
@@ -3,33 +3,41 @@ package com.itmo.java.basics.logic.impl;
 import com.itmo.java.basics.logic.WritableDatabaseRecord;
 
 public class RemoveDatabaseRecord implements WritableDatabaseRecord {
+    private final byte[] key;
+
+    public RemoveDatabaseRecord(byte[] key) {
+        this.key = key;
+    }
+
     @Override
     public byte[] getKey() {
-        return new byte[0];
+        return key;
     }
 
     @Override
     public byte[] getValue() {
-        return new byte[0];
+        return null;
     }
 
     @Override
     public long size() {
-        return 0;
+        return Integer.BYTES + getKeySize() + Integer.BYTES;
     }
 
     @Override
     public boolean isValuePresented() {
+
         return false;
     }
 
     @Override
     public int getKeySize() {
-        return 0;
+        return key.length;
     }
 
     @Override
     public int getValueSize() {
-        return 0;
+
+        return -1;
     }
 }
