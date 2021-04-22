1f10e82e04ded614c75c32b42f0dc538ee73dbeb
ilyaShevchuk <58067954+ilyaShevchuk@users.noreply.github.com>
2021-04-22 11:50:00 +0300

    Lab1 (#1)
    * Commit 1
    
    * Commit 2
    
    * Commit 3
    
    * Commit 4
    
    * Commit 5
    
    * Commit 6
    
    * Commit 7
    
    * Commit 8
    
    * Commit 9
    
    * Commit 10
    
    * Commit 10
    
    * Commit after Elina's corrections
    
    * Commit after Elina's corrections
    
    * Commit after Elina's corrections
    
    * Commit 12
    
    * Commit 12
    
    * Commit 14
    
    * Commit 15
    
    * Commit 15
    
    Co-authored-by: ilyaShevchuk <ilyaShevchuk@users.noreply.github.com>

--- src/main/java/com/itmo/java/basics/logic/impl/TableImpl.java
+++ src/main/java/com/itmo/java/basics/logic/impl/TableImpl.java
@@ -2,9 +2,16 @@ package com.itmo.java.basics.logic.impl;
 
 import com.itmo.java.basics.exceptions.DatabaseException;
 import com.itmo.java.basics.index.impl.TableIndex;
+import com.itmo.java.basics.logic.Segment;
 import com.itmo.java.basics.logic.Table;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
 /**
@@ -17,27 +24,75 @@ import java.util.Optional;
  * и хранящую файлы-сегменты данной таблицы
  */
 public class TableImpl implements Table {
-    static Table create(String tableName, Path pathToDatabaseRoot, TableIndex tableIndex) throws DatabaseException {
-        return null;
+    private final String name;
+    private final Path tableRoot;
+    private final TableIndex tableIndex;
+    private final Map<String, Segment> segments;
+    private Segment lastSegment = null;
+
+    private TableImpl(String tableName, Path pathToDatabaseRoot, TableIndex tbIndex) throws IOException {
+        name = tableName;
+        tableIndex = tbIndex;
+        segments = new HashMap<String, Segment>();
+        Path dir = Paths.get(pathToDatabaseRoot.toString() + File.separator + tableName);
+        Files.createDirectory(dir);
+        tableRoot = dir;
+    }
+
+    public static Table create(String tableName, Path pathToDatabaseRoot, TableIndex tableIndex) throws DatabaseException {
+        try {
+            return new TableImpl(tableName, pathToDatabaseRoot, tableIndex);
+        } catch (IOException e) {
+            throw new DatabaseException("Can not create table", e);
+        }
     }
 
     @Override
     public String getName() {
-        return null;
+
+        return name;
+    }
+
+    public void makeSegmentAvailable() throws DatabaseException {
+        if (lastSegment == null || lastSegment.isReadOnly()) {
+            lastSegment = SegmentImpl.create(SegmentImpl.createSegmentName(name), tableRoot);
+            segments.put(lastSegment.getName(), lastSegment);
+        }
     }
 
     @Override
     public void write(String objectKey, byte[] objectValue) throws DatabaseException {
+        makeSegmentAvailable();
+        try {
+            lastSegment.write(objectKey, objectValue);
+            tableIndex.onIndexedEntityUpdated(objectKey, lastSegment);
 
+        } catch (IOException e) {
+            throw new DatabaseException("Can not write to Table " + name, e);
+        }
     }
 
     @Override
     public Optional<byte[]> read(String objectKey) throws DatabaseException {
-        return Optional.empty();
+        var segment = tableIndex.searchForKey(objectKey);
+        if (segment.isEmpty()) {
+            return Optional.empty();
+        }
+        try {
+            return segment.get().read(objectKey);
+        } catch (IOException e) {
+            throw new DatabaseException("Can not read from segment with name " + segment.get().getName(), e);
+        }
     }
 
     @Override
     public void delete(String objectKey) throws DatabaseException {
-
+        makeSegmentAvailable();
+        try {
+            lastSegment.delete(objectKey);
+        } catch (IOException e) {
+            throw new DatabaseException("Can not add info about delete on segment with name "
+                    + lastSegment.getName(), e);
+        }
     }
 }
