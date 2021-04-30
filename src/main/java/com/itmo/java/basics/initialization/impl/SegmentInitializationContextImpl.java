b12916f7714498b71de1f17f0dcbf2fe33f75ba8
ilyaShevchuk <ilyaShevchuk@users.noreply.github.com>
2021-04-30 14:54:12 +0300

    Commit 1

--- src/main/java/com/itmo/java/basics/initialization/impl/SegmentInitializationContextImpl.java
+++ src/main/java/com/itmo/java/basics/initialization/impl/SegmentInitializationContextImpl.java
@@ -3,39 +3,57 @@ package com.itmo.java.basics.initialization.impl;
 import com.itmo.java.basics.index.impl.SegmentIndex;
 import com.itmo.java.basics.initialization.SegmentInitializationContext;
 
+import java.io.File;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class SegmentInitializationContextImpl implements SegmentInitializationContext {
+    private final String segmentName;
+    private final Path segmentPath;
+    private long currentSize;
+    private Path tablePath;
+    private SegmentIndex index;
+
     public SegmentInitializationContextImpl(String segmentName, Path segmentPath, long currentSize, SegmentIndex index) {
+        this.segmentName = segmentName;
+        this.segmentPath = segmentPath;
+        this.currentSize = currentSize;
+        this.index = index;
     }
 
-    /**
-     * Не используйте этот конструктор. Оставлен для совместимости со старыми тестами.
-     */
     public SegmentInitializationContextImpl(String segmentName, Path tablePath, long currentSize) {
-    }
 
-    public SegmentInitializationContextImpl(String segmentName, Path tablePath) {
-        this(segmentName, tablePath.resolve(segmentName), 0, new SegmentIndex());
+        this.segmentName = segmentName;
+        this.tablePath = tablePath;
+        this.segmentPath = Paths.get(tablePath.toString() + File.separator + segmentName);
+        this.currentSize = currentSize;
+        this.index = new SegmentIndex();
     }
 
     @Override
     public String getSegmentName() {
-        return null;
+
+        return segmentName;
     }
 
     @Override
     public Path getSegmentPath() {
-        return null;
+        return segmentPath;
     }
 
     @Override
     public SegmentIndex getIndex() {
-        return null;
+
+        return index;
     }
 
     @Override
     public long getCurrentSize() {
-        return 0;
+
+        return currentSize;
+    }
+
+    public void updateCurrentSize(int x ){
+        currentSize += x;
     }
 }
